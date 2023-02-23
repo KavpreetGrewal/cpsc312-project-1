03797c2a334bb53a0e0f3dbf50822ca8a1f56de9
Zahra Raza <zahraraza123@gmail.com>
2023-02-22 20:42:34 -0800

    added menu options and functions for createNote

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,7 +4,11 @@ module Lib (
   notesApp
 ) where
 
-import Note (Note(..), createNote, getNoteTitle)
+import Note (Note(..), createNote)
+import User (createUser)
+import UserModel (User(..))
+import Control.Exception (bracket_)
+import System.IO (hFlush, stdout, stdin, hSetEcho, hGetEcho)
 -- import qualified Data.Map as Map
 
 --------------------------------------------------------------------------------------------------------------
@@ -28,7 +32,59 @@ notesApp = do
             user <- createUser username password
             noteTitle <- getNoteTitle
             noteContent <- getNoteContent
-            note <- createNote user noteTitle noteContent
+            createdBy <- getTime
+            note <- createNote user noteTitle noteContent createdBy
             putStrLn "Note created!"
             notesApp
 
+
+getUserOption :: IO MenuOption
+getUserOption = do
+    putStrLn "What would you like to do?"
+    putStrLn "1. Create a note"
+    putStrLn "2. Edit a note"
+    putStrLn "3. Delete a note"
+    putStrLn "4. Quit"
+    input <- getLine
+    case input of
+        "1" -> return CreateNote
+        "2" -> return EditNote
+        "3" -> return DeleteNote
+        "4" -> return Quit
+        _   -> do
+            putStrLn "Invalid input, please try again."
+            getUserOption
+
+getPassword :: IO String
+getPassword = do
+    putStr "Enter password: "
+    hFlush stdout
+    pass <- withEcho False getLine
+    putStrLn ""
+    return pass
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+    old <- hGetEcho stdin
+    bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
+
+
+getNoteTitle :: IO String
+getNoteTitle = do
+    putStrLn "Enter the note title:"
+    getLine
+
+getNoteContent :: IO String
+getNoteContent = do
+    putStrLn "Enter the note content:"
+    getLine
+
+getUsername :: IO String
+getUsername = do
+    putStrLn "Enter your username:"
+    getLine
+
+getTime :: IO String 
+getTime = do
+  putStrLn "Enter the date"
+  getLine
--- src/Note.hs
+++ src/Note.hs
@@ -4,24 +4,13 @@ module Note
     ) where
 
 import UserModel (User)
-import NoteModel (saveNoteToDB)
+import NoteModel (Note(..), saveNoteToDB)
 import Data.List (find, delete)
 
-
-data Note = Note 
-    { title :: String
-    , content :: String
-    } deriving (Show, Eq)
-
-createNote :: User -> String -> String -> IO Note
-createNote user title content = do
-    let note = Note title content
+createNote :: User -> String -> String -> String -> IO Note
+createNote user title content createdBy = do 
+    let note = Note title content createdBy
     -- save note to database 
     saveNoteToDB note
     return note
 
-
-getNoteTitle :: IO String
-getNoteTitle = do
-    putStrLn "Enter the note title:"
-    getLine
